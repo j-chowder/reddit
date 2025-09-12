3e41c6c76bda113fbfd29bdecc527c211a7665fa
Jaden Chou <kiiiiopiuy98@gmail.com>
2025-09-11 17:44:55 -0700

    added the data collection steps

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from dotenv import load_dotenv, dotenv_values
 import os
 import praw
+from collections import deque
+import time
 
 load_dotenv()
 
@@ -12,7 +14,49 @@ reddit = praw.Reddit(
 
 subreddit = reddit.subreddit("frugalmalefashion")
 
-submission = reddit.submission(id='1nef8bv')
+submissions = []
+q = deque()
 
-print(submission.link_flair_text)
+
+        
+### 1. find new submissions
+
+def find_submissions():
+    for submission in subreddit.new(limit=5):
+        print(submission.title), print(submission.id), print(submission.url)
+        if submission.id not in submissions:
+            submissions.append(submission.id)
+            # add the corresponding times in a queue
+            time_posted = submission.created_utc
+            add_times(q, submission.id, time_posted)
+
+### 2. add times
+
+def add_times(q, id, time_posted):
+    for times in [1800, 3600, 7200, 18000, 86400]: # 0.5, 1, 2, 5, 24 hours in seconds since time_posted is in unix.
+        d = {id: time_posted + times}
+        q.append(d)
+
+### 3. check if times have elapsed.
+def check_times():
+    print(q)
+    while time.time() >= list(q[0].values())[0]:
+        record_stats(q[0])
+        q.popleft()
+    
+
+### 4. record stats
+def record_stats(d):
+    id = next(iter(d))
+    print(id)
+    submission = reddit.submission(id=id)
+    
+    submission.score
+    submission.upvote_ratio
+    submission.num_comments
+
+
+
+
+
 
